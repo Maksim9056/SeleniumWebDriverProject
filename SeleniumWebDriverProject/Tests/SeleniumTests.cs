3c9669bc0ba5edab2ee67ed2ecd90ae283579d55
Maksim9056 <bobreczovm@inbox.ru>
2024-01-18 12:29:46 +0300

    Добавьте файлы проекта.

---
+++ SeleniumWebDriverProject/Tests/SeleniumTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Edge;
+namespace SeleniumWebDriverProject.Tests
+{
+    public class SeleniumTests
+    {
+        [Test]
+        public void FirstSeleniumTest()
+        {
+            var edgeOptions = new EdgeOptions();
+
+
+            IWebDriver driver = new EdgeDriver(@"C:\edgedriver_win64\msedgedriver.exe", edgeOptions);
+            //IWebDriver driver = new EdgeDriver();
+            driver.Navigate().GoToUrl("https://localhost:7093");
+            //Assert.That(driver.Title, Is.EqualTo("Selenium"));
+            driver.Quit();
+        }
+    }
+}
